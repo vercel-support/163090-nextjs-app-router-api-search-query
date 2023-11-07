7f9268c76c8061005c9889c7138acc9a721a33e3
William Li <email@williamli.dev>
2023-11-07 19:44:38 +0800

    test api

---
+++ app/api/preview/route.ts
@@ -0,0 +1,9 @@
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  let slug = searchParams.get('slug');
+  return new Response(`Previewing ${slug}`, {
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
+}
