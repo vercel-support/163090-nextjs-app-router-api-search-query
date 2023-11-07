c739360a77501bcf07ff866c9859b93a5709acff
William Li <email@williamli.dev>
2023-11-07 19:48:41 +0800

    add type

--- app/api/preview/route.ts
+++ app/api/preview/route.ts
@@ -1,3 +1,5 @@
+import type { NextRequest } from 'next/server';
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   let slug = searchParams.get('slug');
